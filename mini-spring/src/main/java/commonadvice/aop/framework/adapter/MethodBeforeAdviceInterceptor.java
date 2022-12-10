9cd591a4472e14dc17e2ea37e18e9e00f3fe4c99
huabin <1184884206@qq.com>
2022-12-10 21:54:09 +0800

    mini-spring：[完成]common-advice

---
+++ mini-spring/src/main/java/commonadvice/aop/framework/adapter/MethodBeforeAdviceInterceptor.java
@@ -0,0 +1,25 @@
+package commonadvice.aop.framework.adapter;
+
+import commonadvice.aop.MethodBeforeAdvice;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+/**
+ * @author derekyi
+ * @date 2020/12/6
+ */
+public class MethodBeforeAdviceInterceptor implements MethodInterceptor {
+
+	private MethodBeforeAdvice advice;
+
+	public MethodBeforeAdviceInterceptor(MethodBeforeAdvice advice) {
+		this.advice = advice;
+	}
+
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		//在执行被代理方法之前，先执行before advice操作
+		this.advice.before(invocation.getMethod(), invocation.getArguments(), invocation.getThis());
+		return invocation.proceed();
+	}
+}
--- mini-spring/src/test/java/commonadvice/CommonAdviceTest.java
+++ mini-spring/src/test/java/commonadvice/CommonAdviceTest.java
@@ -1,5 +1,18 @@
 package commonadvice;
 
+import commonadvice.aop.AdvisedSupport;
+import commonadvice.aop.MethodMatcher;
+import commonadvice.aop.TargetSource;
+import commonadvice.aop.aspectj.AspectJExpressionPointcut;
+import commonadvice.aop.framework.CglibAopProxy;
+import commonadvice.aop.framework.JdkDynamicAopProxy;
+import commonadvice.aop.framework.ProxyFactory;
+import commonadvice.aop.framework.adapter.MethodBeforeAdviceInterceptor;
+import commonadvice.common.WorldServiceBeforeAdvice;
+import commonadvice.common.WorldServiceInterceptor;
+import commonadvice.service.WorldService;
+import commonadvice.service.WorldServiceImpl;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -9,9 +22,55 @@ import org.junit.Test;
  */
 public class CommonAdviceTest {
 
+    private AdvisedSupport advisedSupport;
+
+    @Before
+    public void setup() {
+        WorldService worldService = new WorldServiceImpl();
+
+        advisedSupport = new AdvisedSupport();
+        TargetSource targetSource = new TargetSource(worldService);
+        WorldServiceInterceptor methodInterceptor = new WorldServiceInterceptor();
+        MethodMatcher methodMatcher = new AspectJExpressionPointcut("execution(* commonadvice.service.WorldService.explode(..))").getMethodMatcher();
+        advisedSupport.setTargetSource(targetSource);
+        advisedSupport.setMethodInterceptor(methodInterceptor);
+        advisedSupport.setMethodMatcher(methodMatcher);
+    }
+
+    @Test
+    public void testJdkDynamicProxy() throws Exception {
+        WorldService proxy = (WorldService) new JdkDynamicAopProxy(advisedSupport).getProxy();
+        proxy.explode();
+    }
+
+    @Test
+    public void testCglibDynamicProxy() throws Exception {
+        WorldService proxy = (WorldService) new CglibAopProxy(advisedSupport).getProxy();
+        proxy.explode();
+    }
+
+    @Test
+    public void testProxyFactory() throws Exception {
+        // 使用JDK动态代理
+        advisedSupport.setProxyTargetClass(false);
+        WorldService proxy = (WorldService) new ProxyFactory(advisedSupport).getProxy();
+        proxy.explode();
+
+        // 使用CGLIB动态代理
+        advisedSupport.setProxyTargetClass(true);
+        proxy = (WorldService) new ProxyFactory(advisedSupport).getProxy();
+        proxy.explode();
+    }
+
     @Test
     public void testCommonAdvice(){
+        // 设置BeforeAdvice
+        WorldServiceBeforeAdvice beforeAdvice = new WorldServiceBeforeAdvice();
+        MethodBeforeAdviceInterceptor methodInterceptor = new MethodBeforeAdviceInterceptor(beforeAdvice);
+        advisedSupport.setMethodInterceptor(methodInterceptor);
 
+        WorldService proxy = (WorldService) new ProxyFactory(advisedSupport).getProxy();
+        proxy.explode();
     }
 
 }
