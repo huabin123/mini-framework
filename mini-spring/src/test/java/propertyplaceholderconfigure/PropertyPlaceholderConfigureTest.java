cc16d9a0d55599e16e13c94a10b4478b008bb5c5
huabin <1184884206@qq.com>
2022-12-13 09:52:17 +0800

    mini-spring：[完成]PropertyPlaceholderConfigure

--- mini-spring/src/test/java/propertyplaceholderconfigure/PropertyPlaceholderConfigureTest.java
+++ mini-spring/src/test/java/propertyplaceholderconfigure/PropertyPlaceholderConfigureTest.java
@@ -1,9 +1,24 @@
 package propertyplaceholderconfigure;
 
+import org.junit.Test;
+import propertyplaceholderconfigure.bean.Car;
+import propertyplaceholderconfigure.context.support.ClassPathXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @Author huabin
  * @DateTime 2022-12-13 06:59
  * @Desc
  */
 public class PropertyPlaceholderConfigureTest {
+
+    @Test
+    public void testPropertyPlaceholderConfigure() throws Exception {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:property-placeholder-configurer.xml");
+
+        Car car = applicationContext.getBean("car", Car.class);
+        assertThat(car.getBrand()).isEqualTo("lamborghini");
+    }
+
 }
